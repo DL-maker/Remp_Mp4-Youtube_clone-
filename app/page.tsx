6777635149aebfe71a93acacd5a5da3e3ac9e498
DL-maker <laith.dababi@oteria.fr>
2024-12-16 15:22:06 +0100

    Implemented navbar with logo, profile picture and search bar

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+export default function ListeBoutton() {return (
+        <div className='bg-gray-500'>
+            <button className="flex-1 px-4 py-2 text-center hover:bg-gray-300 rounded-md transition-colors duration-200 focus:outline-none flex">
+                <span className="flex-1">Abonnement</span>
+                
+            </button>
+
+        </div>);}
+
+        
