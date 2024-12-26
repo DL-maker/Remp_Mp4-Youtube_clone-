797683c5c54c8c09aacf5870232c3d997a811184
DL-maker <laith.dababi@oteria.fr>
2024-12-26 17:04:21 +0100

    Github Sign-in login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,38 @@
-import LoginForm from './LoginForm'; // Import the LoginForm component
-import SignIn from '@/components/sign-in';
-export default function LoginPage() { // To avoid the server in client error
+"use client";
+import { signIn } from "next-auth/react";
+
+export default function SignIn() {
   return (
-    <div>
-      <LoginForm />
-      <SignIn />
+    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "100vh" }}>
+      <h1>Sign In</h1>
+      <button
+        onClick={() => signIn("google")}
+        style={{
+          margin: "10px",
+          padding: "10px 20px",
+          border: "none",
+          backgroundColor: "#4285F4",
+          color: "white",
+          borderRadius: "5px",
+          cursor: "pointer",
+        }}
+      >
+        Sign in with Google
+      </button>
+      <button
+        onClick={() => signIn("github")}
+        style={{
+          margin: "10px",
+          padding: "10px 20px",
+          border: "none",
+          backgroundColor: "#9146FF",
+          color: "white",
+          borderRadius: "5px",
+          cursor: "pointer",
+        }}
+      >
+        Sign in with github
+      </button>
     </div>
   );
 }
