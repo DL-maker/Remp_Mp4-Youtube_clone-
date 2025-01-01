5f00be977f7ff0ef472c880c0f07dc222f920f5c
DL-maker <laith.dababi@oteria.fr>
2025-01-01 11:08:09 +0100

    setting up the test profile page

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -27,6 +27,13 @@ export default function Navbar() {
                 className="rounded-full h-10 w-10 object-cover cursor-pointer"
               />
             </Link>
+            <Link href="/profile" prefetch={false}>
+              <img
+                src="/icons/bell.svg"
+                alt="Notifications"
+                className="h-6 w-6 object-cover cursor-pointer"
+              />
+            </Link>
           </div>
         </div>
       </div>
