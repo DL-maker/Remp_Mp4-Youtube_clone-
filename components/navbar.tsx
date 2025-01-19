70a44bededfd6edcba44711fafb2420873e89088
DL-maker <laith.dababi@oteria.fr>
2025-01-19 18:30:07 +0100

    Updated sliding column for subscription page

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -129,7 +129,7 @@ const Navbar: React.FC<NavbarProps> = ({ toggleColumn, isOpen }) => {
               </svg>
               <span>Short</span>
             </button>
-            <button className="flex items-center space-x-2 px-4 py-2 bg-gray-200 rounded-md hover:bg-gray-300 transition">
+            <button className="flex items-center space-x-2 px-4 py-2 bg-gray-200 rounded-md hover:bg-gray-300 transition" onClick={() => router.push('/abonnement')}>
               <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" >
                 <path clipRule="evenodd" d="M4 4.5A1.5 1.5 0 015.5 3h13A1.5 1.5 0 0120 4.5H4Zm16.5 3h-17v11h17v-11ZM3.5 6A1.5 1.5 0 002 7.5v11A1.5 1.5 0 003.5 20h17a.5.5 0 00.5-.5v-11A.5.5 0 0020.5 8h-17Zm7.257 4.454a.5.5 0 00-.757.43v4.233a.5.5 0 00.757.429L15 13l-4.243-2.546Z"/>
               </svg>
