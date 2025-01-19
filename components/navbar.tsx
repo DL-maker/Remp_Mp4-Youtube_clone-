8209798f802ffa4814346eb0612d61a32db91920
DL-maker <laith.dababi@oteria.fr>
2025-01-19 20:35:00 +0100

    Put the search bar in the center of the navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -58,13 +58,15 @@ const Navbar: React.FC<NavbarProps> = ({ toggleColumn, isOpen }) => {
               </Link>
             </div>
             
-            <div className="flex items-center space-x-4">
+            <div className="flex-1 flex justify-center ">
               <input
                 type="text"
                 placeholder="Recherche"
-                className="px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                className="px-3 py-2 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500 border-2"
               />
-              
+            </div>
+            
+            <div className="flex items-center space-x-4">
               <Link href="/profile" prefetch={false}>
                 <Image
                   src={"https://thispersondoesnotexist.com"}
