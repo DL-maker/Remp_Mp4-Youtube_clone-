01d76a4c6d5d268c8a823eadbbd7d1de7fd41ea3
DL-maker <laith.dababi@oteria.fr>
2025-05-12 12:19:38 +0200

    Remove the experimental.appDir option as the App Router is stable. Remove the top-level api configuration block

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: ['thispersondoesnotexist.com']
   },
@@ -19,12 +16,6 @@ const nextConfig = {
         ],
       },
     ];
-  },
-  api: {
-    bodyParser: {
-      sizeLimit: '1mb',
-    },
-    responseLimit: false,
   },
 };
 
